815d7ac11ceee45da5854d68e94eabc4d644acbf
Diego DESK <diegocamargo55555@gmail.com>
2025-04-07 09:32:11 -0300

    array to vector, inicio do loop

---
+++ biblioteca.h++
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+struct cidade {
+    std::string nome;
+    std::vector<std::string> vizinhos;
+    std::vector<cidade*> cidades;
+    std::vector<int> distancia;
+};
+int* heuristica(std::vector<cidade> aberto){
+    int* h = new int[2];
+
+    int menor = aberto[0].distancia[0];
+    for (int i = 0; i < aberto.size(); i++)
+    {
+        for (int k = 0; k < aberto[i].distancia.size(); k++)
+        {
+            if (menor > aberto[i].distancia[k])
+            {
+                menor = aberto[i].distancia[k];
+                h[0] = i;
+                h[1] = k;
+            }
+        }       
+    }
+
+    return h;
+    
+}
--- test/test.cc
+++ test/test.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include "biblioteca.h++"
+#include "test.h++"
 using namespace std;
 int main(){
     cidade oradea, zerind, arad, sibiu;
@@ -27,49 +27,49 @@ int main(){
     std::vector<cidade> fechado;
     std::vector<cidade> aberto;
     bool achou = false;
+
     int dist_total = 0;
+
     //int f = g+h;
-
-
-
     aberto.push_back(oradea);
     fechado.push_back(aberto[0]);
     aberto.pop_back();
 
-    for (int i = 0; i < aberto.size(); i++)
+
+
+    for (int i = 0; i < fechado[0].cidades.size(); i++)
     {
-        cout << "a: " << *fechado[0].cidades[i].nome << endl;
         aberto.push_back(*fechado[0].cidades[i]);
     }
-    aberto.push_back(oradea);
 
-
-    
     while (!aberto.empty() )
     {
-        
+        printf("\n---------------");
+
+        printf("\ninicio abertos:");
         for (int i = 0; i < aberto.size(); i++)
         {
-            printf("começo abertos: %s\n", aberto[i].nome.c_str());
+            printf(" %s,", aberto[i].nome.c_str());
         }
 
-        printf("\nola\n");
+        printf("\nola1");
         int* h = heuristica(aberto);
-        printf("h = %d\n", *h);
+        printf("h = %d\n", h[0]);
+        printf("h = %s\n", aberto[h[0]].nome.c_str());
 
         fechado.push_back(aberto[h[0]]);
         aberto.erase(aberto.begin()+h[0]);
 
+        printf("fim abertos:");
         for (int i = 0; i < aberto.size(); i++)
         {
-            printf("fim abertos: %s\n", aberto[i].nome.c_str());
+            printf(" %s,", aberto[i].nome.c_str());
         }
 
+        printf("\nfim fechados:");
         for (int i = 0; i < fechado.size(); i++)
         {
-            printf("fim fechados: %s\n", aberto[i].nome.c_str());
+            printf(" %s,", fechado[i].nome.c_str());
         }
     }  
-
-
 }
